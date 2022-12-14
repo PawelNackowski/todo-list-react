bcb33b7fdefac60cfef12567514e8d4e175d803a
Pawel Nackowski <pawelnackowski@gmail.com>
2022-12-14 10:58:50 +0100

    task list without interactions yet

---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+
+
+const Footer = () => (
+    <footer className="section__footer">
+        Ⓒ Lista zadań stworzona przez Paweł Nackowski (ver. 1.1)
+      </footer>
+);
+
+export default Footer;
---
+++ src/Tasks/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Tasks = (props) => (
+  <ul className="task">
+    {props.tasks.map(task => (
+      <li key={task.id}
+        className={`task__item${task.done && props.hidenDoneTasks ? "task__item--hiden" : ""}`}
+      >
+         <button className="task__button task__button--toggleDone">
+      {task.done ? " ✔" : ""}
+      </button>
+     <span className={`task${task.done ? " task__content--done" : ""}`}>
+       {task.content}
+     </span>
+     <button className="task__button task__button--remove">
+       🗑   
+     </button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Tasks;
