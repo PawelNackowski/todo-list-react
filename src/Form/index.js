468ab453997c2e3938505d78f28919b9d8ad5155
Pawel Nackowski <pawelnackowski@gmail.com>
2023-01-02 11:39:17 +0100

    added spacing on form

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,8 +3,10 @@ import "./style.css";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  
   const onFormSubmit = (event) => {
     event.preventDefault();
+    
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
   };
