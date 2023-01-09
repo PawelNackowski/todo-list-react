92d3ac1f05ec7f9075acb53e5d4b152f97513065
Pawel Nackowski <pawelnackowski@gmail.com>
2023-01-09 14:16:48 +0100

    formated

--- src/styled.js
+++ src/styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const Footer = styled.footer`
-      margin-top: 25px;
-      text-align: center;
-      font-size: 15px;
-    `;
+    margin-top: 25px;
+    text-align: center;
+    font-size: 15px;
+`;
 
 export const Container = styled.main`
     max-width: 900px;
