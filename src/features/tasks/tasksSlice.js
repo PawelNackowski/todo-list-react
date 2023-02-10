609f4ea2dbe6451335ba77bccb8b38c3c3070d02
Pawel Nackowski <pawelnackowski@gmail.com>
2023-02-10 14:54:26 +0100

    renamed payload name

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -7,18 +7,18 @@ const tasksSlice = createSlice({
     hideDone: false,
   },
   reducers: {
-    addTasks: ({ tasks }, { payload }) => {
-      tasks.push(payload);
+    addTasks: ({ tasks }, { payload: task }) => {
+      tasks.push(task);
     },
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: ({ tasks }, { payload }) => {
-      const index = tasks.findIndex(({ id }) => id === payload);
+    toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(({ id }) => id === taskId);
       tasks[index].done = !tasks[index].done;
     },
-    removeTask: ({ tasks, payload }) => {
-      const index = tasks.findIndex(({ id }) => id === payload);
+    removeTask: ({ tasks, payload: findId }) => {
+      const index = tasks.findIndex(({ id }) => id === findId);
       tasks.splice(index, 1);
     },
     setAlldone: ({ tasks }) => {
