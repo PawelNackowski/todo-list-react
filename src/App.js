06088cf03fa9f49134bbe63e624d3fd85ab24721
Pawel Nackowski <pawelnackowski@gmail.com>
2023-02-18 15:44:19 +0100

    added Route on tasks and adding author page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Link, Switch, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min";
+import Author from "./features/author/Author";
 import Tasks from './features/tasks/Tasks';
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch> 
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+)
 
 export default App;
---
+++ src/features/author/Author.js
@@ -0,0 +1,23 @@
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+import { Container, Footer } from "../../styled";
+
+
+const Author = () => (
+  <Container>
+    <Header title="O Autorze" />
+    <Section
+      title="Paweł Nackkowski"
+      body={
+      <>
+      Moją pasją jest fotografia, której poświecam najwięcej czasu. 
+      </>
+      }
+    />
+    <Footer>
+      Ⓒ Lista zadań stworzona przez Paweł Nackowski (ver. 1.1.0)
+    </Footer>
+  </Container>
+);
+
+export default Author;
