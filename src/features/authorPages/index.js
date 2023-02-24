6f89c1b874613d9d4f94785ff72b6e32006297f5
Pawel Nackowski <pawelnackowski@gmail.com>
2023-02-24 12:07:26 +0100

    updated authorPage

--- src/features/authorPages/index.js
+++ src/features/authorPages/index.js
@@ -3,16 +3,17 @@ import Header from "../../common/Header";
 import Section from "../../common/Section";
 import { Container } from "../../styled";
 
-
-
 const Author = () => (
   <Container>
     <Header title="O Autorze" />
     <Section
-      title="Paweł Nackkowski"
+      title="Paweł Nackowski"
       body={
       <>
-      Moją pasją jest fotografia, której poświecam najwięcej czasu. 
+      Moją pasją jest fotografia, której staram sie poświęcać jak najwięcej czasu. <br/>
+      Lubię grać na gitarze, słuchać dobrej muzyki, majserkować i jak starczy czasu obejrzeć dobry film.<br/>
+     Ta <strong>Lista zadań</strong> stworzona jest z myśłą, by ułatwiła Tobie życie. <strong>Żartuję</strong>,<br/>
+     jest to projekt z kursu <strong>You</strong>Code.
       </>
       }
     />
