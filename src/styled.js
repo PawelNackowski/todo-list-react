dfa256c47751e0b8e6d5140a59e1feb41f91c6b0
Pawel Nackowski <pawelnackowski@gmail.com>
2023-02-26 18:07:16 +0100

    created pagages links

--- src/styled.js
+++ src/styled.js
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.main`
@@ -6,34 +5,3 @@ export const Container = styled.main`
     padding: 20px;
     margin: 0 auto;
 `;
-
-export const StyledNavLink = styled(NavLink)`
-    color: ${({ theme }) => theme.color.white};
-    font-size: 15px;
-    margin: 20px;
-    text-decoration: none;
-
-    &:hover{
-        transition: 1s;
-        filter: brightness(80%);
-        text-decoration: underline;
-    }
-
-    &.active{
-        font-weight: bold;
-        font-size: 110%;
-        transition: 1s;
-    }
-`;
-
-export const StyledList = styled.ul`
-    display: flex;
-    height: 50px;
-    justify-content: center;
-    align-items: center;
-    list-style: none;
-    padding: 20px;
-    margin: 0;
-    background-color: ${({ theme }) => theme.color.teal};
-     box-shadow: 0px 10px 5px ${({ theme }) => theme.color.alto};
-`
