728af904cb96d75b344a43b734320c07a2322932
Pawel Nackowski <pawelnackowski@gmail.com>
2023-02-26 18:08:08 +0100

    created Navigation component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
-import Author from "./features/authorPages";
-import TasksPages from "./features/TasksPages";
-import TaskPages from "./features/TasksPages/Tasks/TaskPages";
-
-import { StyledList, StyledNavLink } from "./styled";
+import { HashRouter } from "react-router-dom";
+import PagesLink from "./features/pagesLink";
+import Navigation from "./features/TasksPages/Navigation";
 
 const App = () => (
   <HashRouter>
-    <nav>
-      <StyledList>
-        <li>
-          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/autor">O autorze</StyledNavLink>
-        </li>
-      </StyledList>
-      <Switch>
-        <Route path="/zadania/:id">
-          <TaskPages />
-        </Route>
-        <Route path="/zadania">
-          <TasksPages />
-        </Route>
-        <Route path="/autor">
-          <Author />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/zadania" />
-        </Route>
-      </Switch>
-    </nav>
+    <Navigation />
+    <PagesLink />
   </HashRouter>
 )
 
